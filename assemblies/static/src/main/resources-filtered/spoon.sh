b56a7e74c3c69f0576061e8a1e1a0be017698681
Kenny Urena <kenny.urena@hitachivantara.com>
2023-02-21 14:38:47 -0500

    Spacing issue fix

--- assemblies/static/src/main/resources-filtered/spoon.sh
+++ assemblies/static/src/main/resources-filtered/spoon.sh
@@ -124,7 +124,7 @@ case `uname -s` in
                             fi
 			;;
 
-		arm64)
+    arm64)
   		if $($_PENTAHO_JAVA -version 2>&1 | grep "version \"1\.8\..*" > /dev/null )
                             then
   		  echo "I'm sorry, this Mac platform [$ARCH] is not supported in Java 8"
